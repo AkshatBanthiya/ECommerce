98a3613a8b9fa36f383788a9da2ebaf172b88f17
AkshatBanthiya <akshatbanthiya07@gmail.com>
2021-11-25 21:08:07 +0530

    D-3

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,12 +1,12 @@
 const AdminPage = () => {
-  return (
-      <div>
-          <h1>AdminPage</h1>
-          <div>
-              This is the home page
-          </div>
-      </div>
-  );
+    return (
+        <div>
+            <h1>AdminPage</h1>
+            <div>
+                This is the home page
+            </div>
+        </div>
+    );
 }
 
 export default AdminPage;
--- src/components/MyOrders.js
+++ src/components/MyOrders.js
@@ -1,12 +1,12 @@
 const MyOrders = () => {
-  return (
-      <div>
-          <h1>MyOrders</h1>
-          <div>
-              This is the home page
-          </div>
-      </div>
-  );
+    return (
+        <div>
+            <h1>MyOrders</h1>
+            <div>
+                This is the home page
+            </div>
+        </div>
+    );
 }
 
 export default MyOrders;
