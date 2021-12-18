d4cbd0bf0d103e314b66c9efa5aa2186943e83a2
AkshatBanthiya <akshatbanthiya07@gmail.com>
2021-12-18 12:53:54 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import Home from "./components/Home";
 import Products from "./components/Products";
 import Navbar from "./layout/Navbar";
@@ -8,12 +9,32 @@ import Footer from "./layout/Footer";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
 import Admin from "./components/admin/Admin";
+import { Toaster } from "react-hot-toast";
+import { useDispatch } from 'react-redux';
 function App() {
+
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      // dispatch an action that modifies the store
+      console.log(token)
+      dispatch({
+        type: "SET_AUTH_TOKEN",
+        payload: { token }
+      })
+    }
+
+  }, [])
 
   return (
     <div className="App">
       <Navbar />
       {/* <Home /> */}
+      <Toaster />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/shop' element={<Products />} />
